2048cbf1da4efbcd54449ccc7307aec98e435957
solmi_park <solme214@gmail.com>
2020-01-30 18:00:09 +0900

    render 최적화 실습

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -1,7 +1,20 @@
-import React, {Component} from "react";
+import React, {PureComponent} from "react";
 import './MenuButton.css';
 
-class MenuButton extends Component{
+/**
+ * render 최적화 방안
+ * 1. extends Component -> PureComponent
+ * 2. shouldComponentUpdate 처리
+ */
+class MenuButton extends PureComponent{
+    /*shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if(nextProps.handleMouseDown === this.props.handleMouseDown) {
+            return false;
+        } else {
+            return true;
+        }
+    }*/
+
     render() {
         return(
             <button id={"roundButton"} onMouseDown={this.props.handleMouseDown}></button>
